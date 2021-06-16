2003dde36fb7eb95694475eee09cadf850a15de6
Paulo Clavijo Esteban <paucls@gmail.com>
2021-06-16 21:48:27 +0100

    Update dumbster dependency. Old one does not longer exist.

--- src/test/java/it/xpug/kata/birthday_greetings/AcceptanceTest.java
+++ src/test/java/it/xpug/kata/birthday_greetings/AcceptanceTest.java
@@ -6,6 +6,7 @@ import org.junit.*;
 
 import com.dumbster.smtp.*;
 
+import java.util.List;
 
 public class AcceptanceTest {
 
@@ -30,19 +31,19 @@ public class AcceptanceTest {
 
 		birthdayService.sendGreetings("employee_data.txt", new XDate("2008/10/08"), "localhost", NONSTANDARD_PORT);
 
-		assertEquals("message not sent?", 1, mailServer.getReceivedEmailSize());
-		SmtpMessage message = (SmtpMessage) mailServer.getReceivedEmail().next();
+		assertEquals("message not sent?", 1, mailServer.getReceivedEmails().size());
+		SmtpMessage message = (SmtpMessage) mailServer.getReceivedEmails().get(0);
 		assertEquals("Happy Birthday, dear John!", message.getBody());
 		assertEquals("Happy Birthday!", message.getHeaderValue("Subject"));
-		String[] recipients = message.getHeaderValues("To");
-		assertEquals(1, recipients.length);
-		assertEquals("john.doe@foobar.com", recipients[0].toString());
+		List<String> recipients = message.getHeaderValues("To");
+		assertEquals(1, recipients.size());
+		assertEquals("john.doe@foobar.com", recipients.get(0));
 	}
 
 	@Test
 	public void willNotSendEmailsWhenNobodysBirthday() throws Exception {
 		birthdayService.sendGreetings("employee_data.txt", new XDate("2008/01/01"), "localhost", NONSTANDARD_PORT);
 
-		assertEquals("what? messages?", 0, mailServer.getReceivedEmailSize());
+		assertEquals("what? messages?", 0, mailServer.getReceivedEmails().size());
 	}
 }
